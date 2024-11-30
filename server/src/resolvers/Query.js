b01823ab303a802c31c0c490b409a28c832cb0dd
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-12-01 01:12:09 +0200

    Apollo GraphQl Server

---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,16 @@
+function movies(parent, args) {
+    return {
+        page: 1,
+        totalResults: 10,
+        totalPage: 10,
+        results: [{
+            id: 1,
+            title: 'Movie title',
+            releaseDate: 'release date',
+            posterPath: ''
+        }]
+    }
+}
+module.exports = {
+    movies,
+}
